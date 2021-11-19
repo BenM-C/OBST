02f771901e2d3cef9e929e0d94a9a20b6bb72453
Ben McGuire <bmag2121@gmail.com>
2021-11-19 17:12:03 -0500

    Update ProjectFinalCode.py

--- ProjectFinalCode.py
+++ ProjectFinalCode.py
@@ -102,18 +102,6 @@ def OBST(p,q, output = True):
 
 
 
-# testing examples! taken from the homework to show that this method works.  
-# class/recitation 6
-p=[2,1,4,1]
-q = [1,2,1,1,3]
-#homework 6
-m = [5,3,2,1]
-d = [5,1,1,1,1]
-
-OBST(m,d)
-OBST(p,q)
-
-
 
 
 #===============================================================================================#
@@ -274,4 +262,13 @@ class BST:
         '''
         return None
     
+# testing examples! taken from the homework to show that this method works.  
+# class/recitation 6
+p=[2,1,4,1]
+q = [1,2,1,1,3]
+#homework 6
+m = [5,3,2,1]
+d = [5,1,1,1,1]
 
+OBST(m,d)
+OBST(p,q)
